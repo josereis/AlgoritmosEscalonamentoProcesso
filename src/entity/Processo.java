6f3c8c201dd0fd2b0ac8fe2d2416d7ae835ec320
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2018-04-12 18:54:53 -0300

    Criação do modelo que representa um processo comum.

--- src/entity/Processo.java
+++ src/entity/Processo.java
@@ -1,83 +1,9 @@
 package entity;
 
-/**
- * 
- * @author José Reis Ribeiro Santiago
- * 
- * Classe que tem por objetivo simular um processo que será executado na CPU
- * Os seus principais atributos consistem em armazenar o tempo de chegada, a
- * duração necessária para o processo execultar e o seu ID.
- *
- */
-public class Processo implements Comparable<Processo> {
-	private int id;
-	private int tempoChegada;
-	private int tempoDuracao;
-	private int tempoEntrada;
-	private boolean isFirstResponse;
-	
-	public int getId() {
-		return id;
-	}
-
-	public void setId(int id) {
-		this.id = id;
-	}
-
-	public int getTempoChegada() {
-		return tempoChegada;
-	}
-
-	public void setTempoChegada(int tempoChegada) {
-		this.tempoChegada = tempoChegada;
-	}
-
-	public int getTempoDuracao() {
-		return tempoDuracao;
-	}
-
-	public void setTempoDuracao(int tempoDuracao) {
-		this.tempoDuracao = tempoDuracao;
-	}
-
-	public int getTempoEntrada() {
-		return tempoEntrada;
-	}
-
-	public void setTempoEntrada(int tempoEntrada) {
-		this.tempoEntrada = tempoEntrada;
-	}
-	
-
-	public boolean isFirstResponse() {
-		return isFirstResponse;
-	}
-
-	public void setFirstResponse(boolean isFirstResponse) {
-		this.isFirstResponse = isFirstResponse;
-	}
-
-	@Override
-	public int compareTo(Processo processo) {
-		if(equals(processo)) {
-			return 0;
-		}
-		
-		if(this.tempoDuracao > processo.getTempoDuracao()) {
-			return 1;
-		} else {
-			return -1;
-		}
-	}
+public class Processo extends ProcessoAbstract {
 	
 	public Processo() {
 		
-	}
-	
-	public Processo(int tempoChegada, int tempoDuracao) {
-		this.id = tempoChegada;
-		this.tempoChegada = tempoChegada;
-		this.tempoDuracao = tempoDuracao;
 	}
 	
 }
