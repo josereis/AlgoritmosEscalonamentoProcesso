20ca7db0b2104322da3655d590c49ec554708fa3
José Reis R Santiago <jsribeirosantiago4@gmail.com>
2018-04-14 12:41:29 -0300

    Adicionado a execução do algoritmo da loteria.

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,8 @@
 import java.io.IOException;
-import java.util.LinkedList;
 import java.util.Queue;
 
 import entity.ProcessoAbstract;
+import scheduling_algorithms.Loteria;
 import scheduling_algorithms.PrioridadeDinamica;
 import scheduling_algorithms.RoundRobin;
 import utils.FileManipulation;
@@ -10,7 +10,7 @@ import utils.FileManipulation;
 public class Main {
 	
 	public static void main(String[] args) {
-		RoundRobin RR; PrioridadeDinamica pd;
+		RoundRobin RR; PrioridadeDinamica pd; Loteria lt;
 		try {
 			Queue<ProcessoAbstract> filaProcessos = FileManipulation.getFilaProcessosFile();
 			System.out.println("ROUND ROBIN = " + filaProcessos.size());
@@ -27,7 +27,7 @@ public class Main {
 			System.out.println("TEMPO MEDIO DE ESPERA: " + RR.getTempoMedioEspera());
 			
 			filaProcessos = FileManipulation.getFilaProcessosFile();
-			System.out.println("PRIORIDADE DINAMICA = " + filaProcessos.size());
+			System.out.println("\n\nPRIORIDADE DINAMICA = " + filaProcessos.size());
 			pd = new PrioridadeDinamica(filaProcessos);
 			
 			pd.start();
@@ -39,6 +39,20 @@ public class Main {
 			System.out.println("TEMPO MEDIO DE RETORNO: " + pd.getTempoMedioRetorno());
 			System.out.println("TEMPO MEDIO DE REPOSTA: " + pd.getTempoMedioResposta());
 			System.out.println("TEMPO MEDIO DE ESPERA: " + pd.getTempoMedioEspera());
+			
+			filaProcessos = FileManipulation.getFilaProcessosFile();
+			System.out.println("\n\nLOTERIA = " + filaProcessos.size());
+			lt = new Loteria(filaProcessos);
+			
+			lt.start();
+			
+			lt.setTempoMedioEspera(lt.getTempoMedioEspera()/lt.getNumeroProcessos());
+			lt.setTempoMedioRetorno(lt.getTempoMedioRetorno()/lt.getNumeroProcessos());
+			lt.setTempoMedioResposta(lt.getTempoMedioResposta()/lt.getNumeroProcessos());
+			
+			System.out.println("TEMPO MEDIO DE RETORNO: " + lt.getTempoMedioRetorno());
+			System.out.println("TEMPO MEDIO DE REPOSTA: " + lt.getTempoMedioResposta());
+			System.out.println("TEMPO MEDIO DE ESPERA: " + lt.getTempoMedioEspera());
 		} catch (IOException e) {
 			// TODO Auto-generated catch block
 			System.out.println(e.getMessage());
